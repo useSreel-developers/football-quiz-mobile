61b9ea0b27fdb27e4e75142e6ded9e1161cf1924
jerry-hp <jerry17hutariputra>
2023-11-27 04:51:39 -0800

    add result screen ui

---
+++ components/WinnerBg.tsx
@@ -0,0 +1,13 @@
+import {ImageBackground} from 'react-native';
+
+function WinnerBG({children}: any) {
+  return (
+    <ImageBackground
+      source={require('../assets/winnerBg.png')}
+      style={{flex: 1}}>
+      {children}
+    </ImageBackground>
+  );
+}
+
+export default WinnerBG;
--- screens/ResultScreen.tsx
+++ screens/ResultScreen.tsx
@@ -1,101 +1,203 @@
-import {Pressable, StyleSheet, Text, View, FlatList} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
+import WinnerBg from '../components/WinnerBg';
 import {useRoute} from '@react-navigation/native';
-// import { AntDesign } from "@expo/vector-icons";
 
 const ResultScreen = ({navigation}: any) => {
   const route: any = useRoute();
   const points: any = route?.params?.points;
-
   return (
-    <SafeAreaView style={{margin: 10}}>
+    <WinnerBg>
       <View
         style={{
-          flexDirection: 'row',
+          flex: 1,
+          display: 'flex',
           alignItems: 'center',
-          justifyContent: 'space-between',
+          marginTop: 185,
+          position: 'relative',
+          marginHorizontal: 10,
         }}>
-        <Text>Your Result</Text>
+        <Text style={{color: 'white', fontSize: 25, fontWeight: '900'}}>
+          1 💎 for <Text style={{color: '#669bbc'}}>UseReal</Text>
+        </Text>
         <View
           style={{
-            flexDirection: 'row',
+            display: 'flex',
+            flexDirection: 'column',
             alignItems: 'center',
-            marginRight: 14,
+            position: 'absolute',
+            top: 60,
           }}>
-          <Text>Share</Text>
-          {/* <AntDesign
-            style={{ marginLeft: 4 }}
-            name="sharealt"
-            size={18}
-            color="black"
-          /> */}
+          {/*AVATAR JUARA 1*/}
+          <Image
+            source={{
+              uri: 'https://img.freepik.com/free-vector/happy-boy-character_1308-27606.jpg?size=626&ext=jpg&ga=GA1.1.1818359700.1701086645&semt=ais',
+            }}
+            style={{
+              width: 70,
+              height: 70,
+              backgroundColor: 'red',
+              borderRadius: 50,
+              borderColor: '#ffcc00',
+              borderWidth: 2,
+            }}
+          />
+
+          <Text style={{color: 'white', fontWeight: '900', fontSize: 10}}>
+            NAME OF THE PLAYER
+          </Text>
+          <Text style={{color: 'red', fontWeight: '900'}}>{points}</Text>
+          <Text
+            style={{
+              fontSize: 30,
+              position: 'absolute',
+              top: -25,
+            }}>
+            👑
+          </Text>
+        </View>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            position: 'absolute',
+            left: 10,
+            top: 90,
+          }}>
+          {/*AVATAR JUARA 2*/}
+          <Image
+            source={{
+              uri: 'https://img.freepik.com/free-vector/happy-boy-character_1308-27606.jpg?size=626&ext=jpg&ga=GA1.1.1818359700.1701086645&semt=ais',
+            }}
+            style={{
+              width: 70,
+              height: 70,
+              backgroundColor: 'red',
+              borderRadius: 50,
+              borderColor: '#ffcc00',
+              borderWidth: 2,
+            }}
+          />
+          <Text style={{color: 'white', fontWeight: '900', fontSize: 10}}>
+            NAME OF THE PLAYER
+          </Text>
+          <Text style={{color: 'red', fontWeight: '900'}}>{points}</Text>
+        </View>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            position: 'absolute',
+            right: 10,
+            top: 100,
+          }}>
+          {/*AVATAR JUARA 3*/}
+          <Image
+            source={{
+              uri: 'https://img.freepik.com/free-vector/happy-boy-character_1308-27606.jpg?size=626&ext=jpg&ga=GA1.1.1818359700.1701086645&semt=ais',
+            }}
+            style={{
+              width: 70,
+              height: 70,
+              backgroundColor: 'red',
+              borderRadius: 50,
+              borderColor: '#ffcc00',
+              borderWidth: 2,
+            }}
+          />
+          <Text style={{color: 'white', fontWeight: '900', fontSize: 10}}>
+            NAME OF THE PLAYER
+          </Text>
+          <Text style={{color: 'red', fontWeight: '900'}}>{points}</Text>
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 50,
+            backgroundColor: '#869f00',
+            width: '100%',
+            padding: 10,
+            borderRadius: 15,
+          }}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: 10,
+              alignItems: 'center',
+              backgroundColor: '#004aad',
+              borderRadius: 10,
+              marginBottom: 10,
+              padding: 5,
+            }}>
+            <Image
+              source={{
+                uri: 'https://img.freepik.com/free-vector/happy-boy-character_1308-27606.jpg?size=626&ext=jpg&ga=GA1.1.1818359700.1701086645&semt=ais',
+              }}
+              style={{
+                width: 40,
+                height: 40,
+                backgroundColor: 'red',
+                borderRadius: 50,
+                borderColor: '#ffcc00',
+                borderWidth: 2,
+              }}
+            />
+            <Text style={{color: 'white', fontWeight: '900', fontSize: 12}}>
+              NAMA PERINGKAT 4
+            </Text>
+            <Text
+              style={{
+                color: 'red',
+                fontWeight: '900',
+                fontSize: 12,
+                marginLeft: 'auto',
+              }}>
+              {points}0
+            </Text>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: 10,
+              alignItems: 'center',
+              backgroundColor: '#004aad',
+              borderRadius: 10,
+              padding: 5,
+            }}>
+            <Image
+              source={{
+                uri: 'https://img.freepik.com/free-vector/happy-boy-character_1308-27606.jpg?size=626&ext=jpg&ga=GA1.1.1818359700.1701086645&semt=ais',
+              }}
+              style={{
+                width: 40,
+                height: 40,
+                backgroundColor: 'red',
+                borderRadius: 50,
+                borderColor: '#ffcc00',
+                borderWidth: 2,
+              }}
+            />
+            <Text style={{color: 'white', fontWeight: '900', fontSize: 12}}>
+              NAMA PERINGKAT 5
+            </Text>
+            <Text
+              style={{
+                color: 'red',
+                fontWeight: '900',
+                fontSize: 12,
+                marginLeft: 'auto',
+              }}>
+              {points}0
+            </Text>
+          </View>
         </View>
       </View>
-
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          marginVertical: 10,
-        }}>
-        <Text>Question Answered</Text>
-        <Text>(5/5)</Text>
-      </View>
-
-      <Pressable
-        style={{
-          marginTop: 20,
-          backgroundColor: 'white',
-          borderRadius: 7,
-          height: 200,
-        }}
-        onPress={() => navigation.navigate('Home')}>
-        <Text
-          style={{
-            fontSize: 20,
-            color: 'magenta',
-            textAlign: 'center',
-            marginTop: 20,
-          }}>
-          Score Card
-        </Text>
-        <Text
-          style={{
-            fontSize: 40,
-            color: 'magenta',
-            textAlign: 'center',
-            marginTop: 50,
-          }}>
-          {points}
-        </Text>
-      </Pressable>
-
-      <Pressable
-        style={{
-          alignItems: 'center',
-          marginTop: 50,
-          height: 50,
-          backgroundColor: 'magenta',
-          margin: 60,
-          justifyContent: 'center',
-          borderRadius: 10,
-        }}
-        onPress={() => navigation.navigate('Home')}>
-        <Text
-          style={{
-            color: 'white',
-            // marginTop: 15,
-            fontSize: 20,
-          }}>
-          Play Again
-        </Text>
-      </Pressable>
-    </SafeAreaView>
+    </WinnerBg>
   );
 };
 
 export default ResultScreen;
-
-const styles = StyleSheet.create({});
